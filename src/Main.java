69cf4e967c378f7e1199611c8ca1806c58977e4d
Yasmin <yasmin.rodriguez@gmail.com>
2017-06-08 20:17:19 -0400

    Completed Exercise 1 - Saying Hello

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,16 @@
+import SayingHello.*;
 import TipCalculator.*;
 
 
 /**
  * Used to call all 57 exercises
  */
+
+
 public class Main {
     public static void main (String args[]) {
-        TipCalculator.execute();
-        //TipCalculatorManualTests.execute();
+        //TipCalculator.execute();
+        SayingHello.execute();
     }
     }
 
