2c2edfea451e91f5efd8c8543019d2d7e289ce27
Yasmin <yasmin.rodriguez@gmail.com>
2017-06-13 21:02:28 -0400

    Completed Exercise 6 - Retirement Calculator

--- src/RetirementCalculator/RetirementCalculator.java
+++ src/RetirementCalculator/RetirementCalculator.java
@@ -1,9 +1,10 @@
 package RetirementCalculator;
 import java.util.Scanner;
+import java.time.LocalDate;
 
 /**
  * Calculate how many years are left until retirement and the year of
- * retirement. Use time functions to calculate the year of retirement.
+ * retirement. Use a time function to get the current year.
  * Created by Yasmin on 6/13/2017.
  */
 public class RetirementCalculator {
@@ -14,14 +15,16 @@ public class RetirementCalculator {
         int age = s.nextInt();
         System.out.print("At what age would you like to retire? ");
         int retirementAge = s.nextInt();
-        int yearUntilRetirement = retirementAge - age;
-        System.out.print ("You have " + yearUntilRetirement + " years until you can retire.");
+        int yearsUntilRetirement = retirementAge - age;
+        LocalDate now = LocalDate.now();
+        int currentYear = now.getYear();
+        int retirementYear = currentYear + yearsUntilRetirement;
+        System.out.println ("You have " + yearsUntilRetirement + " years until you can retire.");
+        System.out.println ("It's " + currentYear + " so you can retire in " + retirementYear);
     }
 
     public static void execute(){
         RetirementCalculator calc = new RetirementCalculator();
         calc.calculateRetirement();
     }
-
-
 }
