603d6bb57a52d5f784212e170229ef231fa42584
Yasmin <yasmin.rodriguez@gmail.com>
2017-07-27 21:38:14 -0400

    Refactored switch statement to use default clause.

--- src/ex21monthNumberToNameConverter/MonthNumberToNameConverter.java
+++ src/ex21monthNumberToNameConverter/MonthNumberToNameConverter.java
@@ -20,54 +20,31 @@ public class MonthNumberToNameConverter
         switch (number)
         {
             case 1:
-            {
                 return Month.JANUARY.toString();
-            }
             case 2:
-            {
                 return Month.FEBRUARY.toString();
-            }
             case 3:
-            {
                 return Month.MARCH.toString();
-            }
             case 4:
-            {
                 return Month.APRIL.toString();
-            }
             case 5:
-            {
                 return Month.MAY.toString();
-            }
             case 6:
-            {
                 return Month.JUNE.toString();
-            }
             case 7:
-            {
                 return Month.JULY.toString();
-            }
             case 8:
-            {
                 return Month.AUGUST.toString();
-            }
             case 9:
-            {
                 return Month.SEPTEMBER.toString();
-            }
             case 10:
-            {
                 return Month.OCTOBER.toString();
-            }
             case 11:
-            {
                 return Month.NOVEMBER.toString();
-            }
             case 12:
-            {
                 return Month.DECEMBER.toString();
-            }
+            default:
+                return "You must enter a number from 1 to 12. Try again.";
         }
-        return "You must enter a number from 1 to 12. Try again.";
    }
 }
