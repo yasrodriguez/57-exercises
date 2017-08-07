7104417754f972625a62942191e287638d485653
Yasmin <yasmin.rodriguez@gmail.com>
2017-08-07 08:49:51 -0400

    Ex#29 Initial commit - Completed exercise

---
+++ src/YearsToDoubleInvestment.java
@@ -0,0 +1,15 @@
+/**
+ * Take a rate of return and return how many years it will take to
+ * to double the investment, calculated as: 72/rate.
+ * Constraints: Don't allow 0 or non-numeric values for rate.
+ * Created by Yasmin on 8/7/2017
+ */
+
+public class YearsToDoubleInvestment {
+    public static double calculateYears(double rate){
+        if(rate == 0){
+            throw new ArithmeticException("Rate can't be 0!");
+        }
+        return 72/rate;
+    }
+}
---
+++ src/ex29YearsToDoubleInvestment.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Yasmin on 8/7/2017
+ */
+
+public class ex29YearsToDoubleInvestment {
+}
