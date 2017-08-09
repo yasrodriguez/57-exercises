5c28da03b5e38aa1db1aa80e7f131dbfe88093ed
Yasmin <yasmin.rodriguez@gmail.com>
2017-08-09 09:56:23 -0400

    Refactored Multiplication Tables Generator, so that it prints the tables in tabular format.

--- src/ex30multiplicationTablesGenerator/MultiplicationTablesGenerator.java
+++ src/ex30multiplicationTablesGenerator/MultiplicationTablesGenerator.java
@@ -8,12 +8,22 @@ package ex30multiplicationTablesGenerator;
 
 public class MultiplicationTablesGenerator {
 
-    public static void printTables(){
+    private static void printTables(){
+
+        System.out.print("  ");
+
+        for(int c = 1; c <= 12; c++){
+
+            System.out.printf ("%5s",c);
+        }
+
+        System.out.println();
 
         for (int i = 1; i <= 12; i++){
+            System.out.printf ("%2d", i);
 
             for(int j = 1; j <= 12; j++){
-                System.out.println (i + " x " + j + " = " + (i * j));
+                System.out.printf ("%5d", i * j);
             }
 
             System.out.println();
